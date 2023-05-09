a98756f5c99e0c47d64e8c72284922c187a66eb2
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-05-09 22:30:03 +0500

    erase

---
+++ script.js
@@ -0,0 +1,9 @@
+// erase data value
+document.getElementById('erase').onclick = function () {
+    document.getElementById('data').value = "";
+}
+
+//  output erase 
+document.getElementById('Clean').onclick = function () {
+    document.getElementById('output').innerHTML = "";
+}
