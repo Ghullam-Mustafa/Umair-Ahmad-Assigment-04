c45f2132ca8e8263f740819b6346cc22790d4d2d
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-05-09 23:41:33 +0500

    convert

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ document.getElementById('erase').onclick = function () {
 document.getElementById('Clean').onclick = function () {
     document.getElementById('output').innerHTML = "";
 }
+
+// orignal cities
+let orignalCities = "I love Pakistan <br> I love Faisalabad <br> I love Motherland"
+
+
+//  Convert lowwer case 
+document.getElementById('Lowercase').onclick = function () {
+    let Lowercase = orignalCities.toLocaleLowerCase()
+
+    document.getElementById('output').innerHTML = Lowercase
+}
+
+// convert Upper case
+document.getElementById('Uppercase').onclick = function () {
+    
+    let Uppercase = orignalCities.toLocaleUpperCase()
+
+    document.getElementById('output').innerHTML = Uppercase
+}
+
+// convert Capitalize
+
+document.getElementById('Capitalize').onclick = function () {
+    let convertCapitalize = '<span style= "text-transform = capitalize"; >' + orignalCities + '</span>'
+    document.getElementById('output').innerHTML = convertCapitalize
+}
+
+// convert lowerlize
+
+document.getElementById('lowercase').onclick = function () {
+    let convertCapitalize1 = '<span style= "text-transform = uppercase"; >' + orignalCities + '</span>'
+    document.getElementById('output').innerHTML = convertCapitalize1
+
+}
